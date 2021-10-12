41c99e7a4a19c8e568e28f714cd6d434ca8aea8d
derikvanschaik <derikvanschaik@gmail.com>
2021-10-12 16:10:13 -0700

    bounding box features implemented, new line in the works

--- app.js
+++ app.js
@@ -13,20 +13,44 @@ window.onload = () =>{
     const canvas = document.getElementById("mimi-canvas");
     const ctx = canvas.getContext("2d");
     // configurations 
-    ctx.font = "20px Georgia"; 
+    ctx.font = "15pt Comic Sans MS";  
     // global variables 
     let clickX, clickY;  
     
-    // event handlers 
+    // event handlers
+    window.addEventListener("keydown" , (event) =>{
+        console.log(event.key); 
+        if (event.key === "Enter"){
+            console.log("here");
+            // SHOVE THIS REUSED CODE INTO A FUNCTION ALSO ON LINE 47 
+            const metrics = ctx.measureText(userInput.value);
+            const width = metrics.width;
+            // thank you to https://erikonarheim.com/posts/canvas-text-metrics/ for this 
+            const height = Math.abs(metrics.fontBoundingBoxAscent) + Math.abs(metrics.fontBoundingBoxDescent);
+            clickY += height; // append to new line 
+        }
+    }); 
     canvas.addEventListener("click", (event)=>{
         const [x,y] = getCursorPosition(canvas, event);
         [clickX, clickY] = [x, y];
-        clearInput(userInput); // in case there was previous input 
+
+        if (userInput.value.length > 0){
+            clearInput(userInput);
+        }
         userInput.focus();  
     });
 
     userInput.addEventListener("input", ()=>{
+        
+        const metrics = ctx.measureText(userInput.value); 
+        const width = metrics.width;
+        // thank you to https://erikonarheim.com/posts/canvas-text-metrics/ for this 
+        const height = Math.abs(metrics.fontBoundingBoxAscent) + Math.abs(metrics.fontBoundingBoxDescent);
+        // clear previous rect 
+        ctx.clearRect(clickX, clickY + 10, width, -height);   
+        // fill new rect and text 
         ctx.fillText(userInput.value, clickX, clickY); 
+        ctx.strokeRect(clickX, clickY + 10, width, -height);    
     }); 
 
 }
