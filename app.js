06863fcc6c21577929b4886d06a1d90f68600e32
derikvanschaik <derikvanschaik@gmail.com>
2021-10-12 18:19:18 -0700

    creating textobject class

--- app.js
+++ app.js
@@ -7,45 +7,68 @@ const getCursorPosition = (canvas, event) => {
 const clearInput = (inputEl) =>{
     inputEl.value = ""; 
 }
+// returns width and height of text 
+const getFontDimensions = (ctx, text) =>{
+    const metrics = ctx.measureText(text);  
+    const width = metrics.width;
+    const height = Math.abs(metrics.fontBoundingBoxAscent) + Math.abs(metrics.fontBoundingBoxDescent);
+    return [width, height];  
+}
+class TextObject{
+    constructor(x, y){ 
+        this.x = x; 
+        this.y = y;
+        this.currentLine = 0;
+        this.lines = []; 
+    }
+    getInfo(){
+        console.log(this.x, this.y, this.currentLine, this.lines); 
+    }
+}
 
 window.onload = () =>{
     const userInput = document.querySelector("input"); 
     const canvas = document.getElementById("mimi-canvas");
     const ctx = canvas.getContext("2d");
-    // configurations 
-    ctx.font = "15pt Comic Sans MS";  
-    // global variables 
-    let clickX, clickY;  
+
+    // Canvas configurations 
+    ctx.font = "15pt Comic Sans MS";
+
+    // global variables
+    let clickX, clickY;
+    let textObjects = []; 
     
     // event handlers
     window.addEventListener("keydown" , (event) =>{
-        console.log(event.key); 
         if (event.key === "Enter"){
-            console.log("here");
-            // SHOVE THIS REUSED CODE INTO A FUNCTION ALSO ON LINE 47 
-            const metrics = ctx.measureText(userInput.value);
-            const width = metrics.width;
-            // thank you to https://erikonarheim.com/posts/canvas-text-metrics/ for this 
-            const height = Math.abs(metrics.fontBoundingBoxAscent) + Math.abs(metrics.fontBoundingBoxDescent);
+            const [_, height] = getFontDimensions(ctx, userInput.value);   
             clickY += height; // append to new line 
         }
     }); 
     canvas.addEventListener("click", (event)=>{
+
         const [x,y] = getCursorPosition(canvas, event);
         [clickX, clickY] = [x, y];
 
         if (userInput.value.length > 0){
             clearInput(userInput);
         }
+        const existingTextObject = textObjects.find(textObj => textObj.x === x && textObj.y === y); 
+        if (!existingTextObject){
+            textObjects.push(new TextObject(x, y) ); 
+        }
         userInput.focus();  
     });
 
     userInput.addEventListener("input", ()=>{
         
-        const metrics = ctx.measureText(userInput.value); 
-        const width = metrics.width;
-        // thank you to https://erikonarheim.com/posts/canvas-text-metrics/ for this 
-        const height = Math.abs(metrics.fontBoundingBoxAscent) + Math.abs(metrics.fontBoundingBoxDescent);
+        const existingTextObject = textObjects.find(textObj => textObj.x === clickX && textObj.y === clickY); 
+        if (existingTextObject){
+            existingTextObject.getInfo(); 
+        }else{
+            console.log("no existing text object"); 
+        }
+        const [width, height] = getFontDimensions(ctx, userInput.value); 
         // clear previous rect 
         ctx.clearRect(clickX, clickY + 10, width, -height);   
         // fill new rect and text 
